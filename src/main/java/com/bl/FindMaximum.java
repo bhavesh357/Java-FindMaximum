24054ab53eef00035df26207adacb877e011e682
Bhavesh Kadam <bkadam357@gmail.com>
2020-04-27 20:34:59 +0530

    ADDED: test to validate max number

--- src/main/java/com/bl/FindMaximum.java
+++ src/main/java/com/bl/FindMaximum.java
@@ -2,4 +2,13 @@ package com.bl;
 
 public class FindMaximum {
 
+    public Integer getMaximum(Integer[] array){
+        int max = array[0];
+        for(int i=1;i<array.length;i++){
+            if(array[i].compareTo(max)>0){
+                max=array[i];
+            }
+        }
+        return max;
+    }
 }
--- src/test/java/com/bl/FindMaximumTest.java
+++ src/test/java/com/bl/FindMaximumTest.java
@@ -1,5 +1,21 @@
 package com.bl;
 
-public class FindMaximumTest {
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
+public class FindMaximumTest {
+    static FindMaximum findMaximum;
+
+    @Before
+    public void Initialize() {
+        findMaximum = new FindMaximum();
+    }
+
+    @Test
+    public void whenGivenThreeInteger_ShouldReturnMaximum() {
+        Integer max=findMaximum.getMaximum(new Integer[]{2,4,6});
+        Assert.assertSame(6,max);
+    }
 }
